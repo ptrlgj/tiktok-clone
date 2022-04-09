cc4f8b52439213980f7d61504d51106a1901c6b9
ptrlgj <piotr.ligaj24@gmail.com>
2022-04-09 20:59:44 +0200

    basic components, instalation netlify-cli

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Upload from './pages/Upload';
+import Header from './components/Header'
 function App() {
   return (
     <BrowserRouter>
+        <Header />
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/upload" element={<Upload />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import FollowersColumn from '../components/FollowersColumn'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='container'>
+        <FollowersColumn />
+        <div className='feed'>
+            <h1>Home</h1>
+        </div>
+        <div className='suggested-box'></div>
+    </div>
   )
 }
 
