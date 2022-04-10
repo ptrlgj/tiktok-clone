690f6d7a714aaab42ff0b5f8f10d50a5b82c3d2c
ptrlgj <piotr.ligaj24@gmail.com>
2022-04-10 10:46:30 +0200

    setting up db connection

--- functions/addData.js
+++ functions/addData.js
@@ -1,1 +1,15 @@
-const { createClient } = require("@astrajs/collections")
+const { createClient } = require("@astrajs/collections");
+
+const collection = 'posts';
+
+exports.handler = async function (event, context, callback) {
+    const astraClient = await createClient({
+        astraDatabaseId: process.env.ASTRA_DB_ID,
+        astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+        applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+    });
+
+    const posts = astraClient
+        .namespace(process.env.ASTRA_DB_KEYSPACE)
+        .collection(collection)
+}
